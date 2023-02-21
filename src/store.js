e59fefd3234fe86fb092ef3fe09f27370e1e6de5
Belloshehu <belloshehu1@gmail.com>
2023-02-21 11:35:38 +0100

    Add redux store for movie

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+import movieSlice from './features/movie/movieSlice'
+
+
+export const store = configureStore({
+    reducer: {
+        movie: movieSlice
+    }
+})
