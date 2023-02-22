0fb829cdd3e7c1f03a193a4333bafd6030974929
Belloshehu <belloshehu1@gmail.com>
2023-02-22 19:33:11 +0100

    update movie detail page

---
+++ src/components/Favourites/Favourites.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import { FaHeart } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+import './Favourites.css'
+
+const Favourites = () => {
+    const {favourites} = useSelector(store => store.movie)
+  return (
+    <div className='relative group'>
+        <FaHeart className='fav-icon'/>
+        <span className='fav-number'>{favourites}</span>
+    </div>
+  )
+}
+
+export default Favourites
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FaHeart } from 'react-icons/fa'
+import Favourites from '../Favourites/Favourites'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <header className='header text-green-400'>
-        <h2 className='text-red-500 text-center font-bold'>MovieApp</h2>
+    <header className='header text-white flex justify-between items-center p-2 bg-indigo-500 lg:px-20'>
+        <h2 
+          className='text-center text-3xl font-bold'
+          >
+            <span className='bg-white text-indigo-900 p-2 shadow-lg shadow-black rounded-md'>M</span>Moviex
+        </h2>
+        <Favourites />
     </header>
   )
 }
